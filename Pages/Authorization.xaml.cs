757a3d283b997a54be6c2af32150703fe012c584
admin <admin@DESKTOP-2N8JR3F>
2022-12-26 12:56:42 +0300

    Добавьте файлы проекта.

---
+++ Pages/Authorization.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Gruzdev.Pages;
+using Gruzdev.DataBase;
+
+namespace Gruzdev.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Page
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var ContextUser = TradeEntities.GetContext().User;
+            var Autho = ContextUser.Where(p => p.UserLogin == UsLog.Text && p.UserPassword == UsPass.Text).FirstOrDefault();
+            if (Autho != null)
+                NavigationService.Navigate(new Capcha());
+            else MessageBox.Show("Вы ввели некорректные данные","Упс...");
+
+
+
+        }
+
+        private void SignInGuessBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Service());
+        }
+    }
+}
---
+++ Pages/Capcha.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Gruzdev.Pages;
+using Gruzdev.DataBase;
+
+namespace Gruzdev.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Capcha.xaml
+    /// </summary>
+    public partial class Capcha : Page
+    {
+        public Capcha()
+        {
+            InitializeComponent();
+
+            GenerationCaphca();
+        }
+
+        private void OutPutCapcha_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void CapchaBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (InPutCapcha == OutPutCapcha)
+                NavigationService.Navigate(new Service());
+           
+
+
+        }
+
+        public void GenerationCaphca()
+        {
+            Random rand = new Random();
+            OutPutCapcha.Text = "";
+            for (int i = 0; i < 5; i++)
+                OutPutCapcha.Text += (char)('a') + (rand.Next(26));
+        }
+          
+
+
+
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            GenerationCaphca();
+        }
+    }
+}
